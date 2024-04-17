b0816d848264cc2694b968454d1b729975d89393
hetricke <hetricke@umich.edu>
2024-04-17 15:29:19 -0400

    successfully convert input data to numpy arrays; code fails to to guassian nb requiring numberic input data

--- movie_review_analysis.py
+++ movie_review_analysis.py
@@ -14,7 +14,7 @@ nltk.download('punkt')
 """Prepare the dataset before training"""
 
 # 1.1 Load dataset
-dataset = pd.read_csv('Dataset/IMDB.csv', encoding_errors='replace')
+dataset = pd.read_csv('Dataset/IMDB.csv', encoding_errors='replace', nrows=5)
 print(f"Dataset shape : {dataset.shape}\n")
 print(f"Dataset head : \n{dataset.head()}\n")
 
@@ -52,22 +52,37 @@ def remove_stopwords(tokens):
 dataset.review = dataset.review.apply(remove_stopwords)
 print(f"Review sample after removing stopwords : \n{dataset.review[0]}\n")
 
+#2.4 Convert review to a numpy array and pad
+SET_SIZE = 200
+def set_size(word_list):
+    if len(word_list) >= SET_SIZE:
+        return np.array(word_list[:SET_SIZE])
+    
+    else:
+        word_list += [''] * (SET_SIZE - len(word_list))
+        temp_array = np.array(word_list)
+        return temp_array
+
+dataset.review = dataset.review.apply(set_size)
+print(f"Review sample after converting to numpy array : \n{dataset.review[0]}\n")
+
 ## 3 | Model Creation ##
 """Create model to fit it to the data"""
 
 # 3.1 Creating Bag Of Words (BOW)
-x = np.array(dataset.iloc[:,0].values)
-y = np.array(dataset.sentiment.values)
+x_data = np.vstack(dataset.review)
+y_data = dataset.sentiment.to_numpy()
 #X = cv.fit_transform(dataset.review).toarray()
 print(f"=== Bag of words ===\n")
-print(f"BOW X shape : {x.shape}")
-print(f"BOW y shape : {y.shape}\n")
+print(f"BOW X shape : {x_data.shape}")
+print(f"BOW y shape : {y_data.shape}\n")
 
 #3.2 Split data into train and test sets
-x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.5, random_state=0)
+x_train, x_test, y_train, y_test = train_test_split(x_data, y_data, test_size=0.5, random_state=0)
 
 #3.3 Train Bayes Network
 gauss_nb = GaussianNB()
-y_pred = gauss_nb.fit(x_train, y_train).predict(x_test)
+trained_model = gauss_nb.fit(x_train, y_train)
+y_pred = trained_model.predict(x_test)
 
 print("Number of mislabeled points out of a total %d points : %d" % (x_test.shape[0], (y_test != y_pred).sum()))
